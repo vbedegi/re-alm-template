59ef668d4d28da4a89a6d53652a7708acfe8a028
vbedegi <vbedegi@gmail.com>
2017-11-11 12:43:24 +0100

    using re-alm 0.3.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-alm/lein-template "0.1.7"
+(defproject re-alm/lein-template "0.1.8"
   :description "re-alm project template for Leiningen"
   :url "https://github.com/vbedegi/re-alm-template"
   :license {:name "Eclipse Public License"
